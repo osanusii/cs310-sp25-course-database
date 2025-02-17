a6c5e0c8fd3c6ae057b6fda91413553015d0a65c
osanusii <sanusiolamilekan18@gmail.com>
2025-02-17 16:23:11 -0600

    DAOFactory Code Done!

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOFactory.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOFactory.java
@@ -1,62 +1,58 @@
 package edu.jsu.mcis.cs310.coursedb.dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public final class DAOFactory {
-    
-    private static final String PROPERTY_URL = "url";
-    private static final String PROPERTY_DRIVER = "driver";
-    private static final String PROPERTY_USERNAME = "username";
-    private static final String PROPERTY_PASSWORD = "password";
-    
-    private final String url, username, password;
-    private Connection conn;
-    
-    public DAOFactory(String prefix) {
+   private static final String PROPERTY_URL = "url";
+   private static final String PROPERTY_DRIVER = "driver";
+   private static final String PROPERTY_USERNAME = "username";
+   private static final String PROPERTY_PASSWORD = "password";
+   private final String url;
+   private final String username;
+   private final String password;
+   private Connection conn;
 
-        DAOProperties properties = new DAOProperties(prefix);
+   public DAOFactory(String prefix) {
+      DAOProperties properties = new DAOProperties(prefix);
+      this.url = properties.getProperty("url");
+      this.username = properties.getProperty("username");
+      this.password = properties.getProperty("password");
 
-        this.url = properties.getProperty(PROPERTY_URL);
-        this.username = properties.getProperty(PROPERTY_USERNAME);
-        this.password = properties.getProperty(PROPERTY_PASSWORD);
-        
-        try {
-            conn = DriverManager.getConnection(url, username, password);
-        }
-        catch (Exception e) {
-            conn = null;
-            e.printStackTrace();
-        }
-        
-    }
-    
-    Connection getConnection() {
-        return conn;
-    }
-    
-    public boolean isClosed() {
-        
-        boolean isClosed = true;
-        
-        try {
-            isClosed = conn.isClosed();
-        }
-        catch (Exception e) { e.printStackTrace(); }
-        
-        return isClosed;
-        
-    }
-    
-    public RegistrationDAO getRegistrationDAO() {
-        return new RegistrationDAO(this);
-    }
-    
-    public SectionDAO getSectionDAO() {
-        return new SectionDAO(this);
-    }
-    
-    public StudentDAO getStudentDAO() {
-        return new StudentDAO(this);
-    }
-    
+      try {
+         this.conn = DriverManager.getConnection(this.url, this.username, this.password);
+      } catch (Exception var4) {
+         this.conn = null;
+         var4.printStackTrace();
+      }
+
+   }
+
+   Connection getConnection() {
+      return this.conn;
+   }
+
+   public boolean isClosed() {
+      boolean isClosed = true;
+
+      try {
+         isClosed = this.conn.isClosed();
+      } catch (Exception var3) {
+         var3.printStackTrace();
+      }
+
+      return isClosed;
+   }
+
+   public RegistrationDAO getRegistrationDAO() {
+      return new RegistrationDAO(this);
+   }
+
+   public SectionDAO getSectionDAO() {
+      return new SectionDAO(this);
+   }
+
+   public StudentDAO getStudentDAO() {
+      return new StudentDAO(this);
+   }
 }
