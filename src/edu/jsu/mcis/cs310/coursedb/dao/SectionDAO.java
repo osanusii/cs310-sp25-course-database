248efcf3bcfa9759e5d37e57c669a0f3fdb191e9
osanusii <sanusiolamilekan18@gmail.com>
2025-02-17 16:29:57 -0600

    SectionDAO Code Done!

--- src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
@@ -1,51 +1,66 @@
 package edu.jsu.mcis.cs310.coursedb.dao;
 
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 
 public class SectionDAO {
-    
-    private static final String QUERY_FIND = "SELECT * FROM section WHERE termid = ? AND subjectid = ? AND num = ? ORDER BY crn";
-    
-    private final DAOFactory daoFactory;
-    
-    SectionDAO(DAOFactory daoFactory) {
-        this.daoFactory = daoFactory;
-    }
-    
-    public String find(int termid, String subjectid, String num) {
-        
-        String result = "[]";
-        
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        ResultSetMetaData rsmd = null;
-        
-        try {
-            
-            Connection conn = daoFactory.getConnection();
-            
-            if (conn.isValid(0)) {
-                
-                // INSERT YOUR CODE HERE
-                
+   private final DAOFactory daoFactory;
+
+   SectionDAO(DAOFactory daoFactory) {
+      this.daoFactory = daoFactory;
+   }
+
+   public String find(int termid, String subjectid, String num) {
+      String result = "[]";
+      JsonArray jsonArray = new JsonArray();
+      PreparedStatement ps = null;
+      ResultSet rs = null;
+      ResultSetMetaData rsmd = null;
+
+      try {
+         Connection conn = this.daoFactory.getConnection();
+         if (conn.isValid(0)) {
+            String QUERY_FIND = "SELECT * FROM section WHERE termid = ? AND subjectid = ? AND num = ? ORDER BY crn";
+            ps = conn.prepareStatement(QUERY_FIND);
+            ps.setInt(1, termid);
+            ps.setString(2, subjectid);
+            ps.setString(3, num);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+               JsonObject jsonObject = new JsonObject();
+               jsonObject.put("termid", rs.getString("termid"));
+               jsonObject.put("num", rs.getInt("num"));
+               jsonObject.put("subjectid", rs.getInt("subjectid"));
+               jsonArray.add(jsonObject);
             }
-            
-        }
-        
-        catch (Exception e) { e.printStackTrace(); }
-        
-        finally {
-            
-            if (rs != null) { try { rs.close(); } catch (Exception e) { e.printStackTrace(); } }
-            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
-            
-        }
-        
-        return result;
-        
-    }
-    
+         }
+      } catch (Exception var24) {
+         var24.printStackTrace();
+      } finally {
+         if (rs != null) {
+            try {
+               rs.close();
+            } catch (Exception var23) {
+               var23.printStackTrace();
+            }
+         }
+
+         if (ps != null) {
+            try {
+               ps.close();
+            } catch (Exception var22) {
+               var22.printStackTrace();
+            }
+         }
+
+      }
+
+      String sizeAsString = Integer.toString(jsonArray.size());
+      return sizeAsString;
+   }
 }
